b897a3851cbe70657f25c279e1095a1eab466d98
Ryan Bravo <rrbravo2@gmail.com>
2019-06-01 17:42:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,31 @@
 def consolidate_cart(cart)
-  # code here
+  consolidated_cart = {}
+  cart.each do |data|
+    data.each do |item, details|
+      if consolidated_cart.keys.include?(item)
+        consolidated_cart[item][:count] += 1
+   else
+     consolidated_cart[item] = details
+     consolidated_cart[item][:count] = 1
+      end
+    end
+  end
+  consolidated_cart
 end
 
+
 def apply_coupons(cart, coupons)
-  # code here
+
+
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, price_hash|
+    if price_hash[:clearance] == true
+      price_hash[:price] = (price_hash[:price] * 0.8).round(2)
+    end# code here
+   end
+  cart
 end
 
 def checkout(cart, coupons)
