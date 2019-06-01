5ee36e075473aa83f81d86c5ea61311ae7488c4b
Ryan Bravo <rrbravo2@gmail.com>
2019-06-01 18:29:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,9 +19,11 @@ def apply_coupons(cart, coupons)
    coupon_name = coupon[:item]
    coupon_item_number = coupon[:number]
    cart_item = cart[coupon_name]
-   next if cart_item.nil? || cart_item[:count] < coupon_item_number
+
+    if cart_item.nil? || cart_item[:count] < coupon_item_number
    cart_item[:count] -= coupon_item_number
    coupon_in_cart = cart["#{coupon_name} W/COUPON"]
+
    if coupon_in_cart
      coupon_in_cart[:count] += 1
       else cart["#{coupon_name} W/COUPON"] = {price: coupon[:cost], clearance: cart_item[:clearance], count: 1}
