57742d604a5d90fabf068e9bc2400ef302d6509c
Ryan Bravo <rrbravo2@gmail.com>
2019-06-01 18:41:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,7 +48,7 @@ def checkout(cart, coupons)
   cart2 = apply_clearance(cart1)
   total = 0
   cart2.each do |name, price_hash|
-    total += price_hash[:price] * price_hash[:count]
+    total += (price_hash[:price] * price_hash[:count]).to_f
   end
   total > 100 ? total * 0.9 : total# code here
 end
